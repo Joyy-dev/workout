0266c8d07cac8bf711ae9c89677b3957458f3ece
joyy_dev <joyceava874@gmail.com>
2026-03-02 13:07:14 +0100

    graph functionalities

--- lib/widgets/workout_chart.dart
+++ lib/widgets/workout_chart.dart
@@ -1,13 +1,57 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class WorkoutChart extends StatelessWidget {
+enum TimeFrame {daily, monthly, yearly}
+
+class WorkoutChart extends StatefulWidget {
   const WorkoutChart({super.key});
+
+  @override
+  State<WorkoutChart> createState() => _WorkoutChartState();
+}
+
+class _WorkoutChartState extends State<WorkoutChart> {
+  TimeFrame selectedTimeFrame = TimeFrame.monthly;
+
+  List<double> chartData = [];
+  double getMaxY() {
+    return chartData.reduce((a, b) => a > b ? a : b) + 2;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadChartData();
+  }
+
+  void loadChartData() {
+    if (selectedTimeFrame == TimeFrame.daily) {
+      chartData = [2, 5, 4,6, 3, 8, 6, 3];
+    } else if (selectedTimeFrame == TimeFrame.monthly) {
+      chartData = [20, 35, 28, 10, 30, 45, 38, 25, 50, 42, 30, 48];
+    } else {
+      chartData = [200, 350, 280, 400, 300,];
+    }
+    setState(() {
+      //
+    });
+  }
+
+  List<String> getBottomTiles() {
+    if (selectedTimeFrame == TimeFrame.daily) {
+      return ['Sun','Mon','Tue', 'Wed', 'Thu','Fri','Sat'];
+    } else if (selectedTimeFrame == TimeFrame.monthly) {
+      return ['Jan','Feb','Mar', 'Apr', 'May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    } else {
+      int currentYear = DateTime.now().year;
+      return List.generate(5, (index) => (currentYear - 4 + index).toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 320,
       width: double.infinity,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -28,13 +72,26 @@ class WorkoutChart extends StatelessWidget {
                     )
                   ],
                 ),
-                Row(
-                  children: [
-                    Text(
-                      'Monthly'
+                DropdownButton<TimeFrame>(
+                  value: selectedTimeFrame,
+                  items: const [
+                    DropdownMenuItem(
+                      value: TimeFrame.daily,
+                      child: Text('Daily')
                     ),
-                    Icon(Icons.arrow_drop_down)
-                  ],
+                    DropdownMenuItem(
+                      value: TimeFrame.monthly,
+                      child: Text('Monthly')
+                    ),
+                    DropdownMenuItem(
+                      value: TimeFrame.yearly,
+                      child: Text('Yearly')
+                    )
+                  ], 
+                  onChanged: (value) {
+                    selectedTimeFrame = value!;
+                    loadChartData();
+                  }
                 )
               ],
             ),
@@ -45,6 +102,7 @@ class WorkoutChart extends StatelessWidget {
               height: 230,
               child: BarChart(
                 BarChartData(
+                  maxY: getMaxY(),
                   borderData: FlBorderData(show: false),
                   alignment: BarChartAlignment.spaceAround,
                   gridData: FlGridData(show: false),
@@ -62,35 +120,50 @@ class WorkoutChart extends StatelessWidget {
                       sideTitles: SideTitles(
                         showTitles: true,
                         getTitlesWidget: (value, meta) {
-                          const months = [
-                            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-                          ];
-                          if (value.toInt() < 0 || value.toInt() >= months.length) {
-                            return SizedBox.shrink();
+                          final titles = getBottomTiles();
+              
+                          if (value.toInt() < 0 || value.toInt() >= titles.length) {
+                            return const SizedBox.shrink();
                           }
+              
                           return Text(
-                            months[value.toInt()],
+                            titles[value.toInt()],
                             style: TextStyle(
                               fontSize: 12
                             ),
                           );
                         },
+                        // getTitlesWidget: (value, meta) {
+                        //   final 
+                        //   // const months = [
+                        //   //   'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+                        //   // ];
+                        //   // if (value.toInt() < 0 || value.toInt() >= months.length) {
+                        //   //   return SizedBox.shrink();
+                        //   // }
+                        //   // return Text(
+                        //   //   months[value.toInt()],
+                        //   //   style: TextStyle(
+                        //   //     fontSize: 12
+                        //   //   ),
+                        //   // );
+                        // },
                       )
                     )
                   ),
-                  barGroups: List.generate(12, (index) {
+                  barGroups: List.generate(chartData.length, (index) {
                     return BarChartGroupData(
                       x: index,
                       barRods: [
                         BarChartRodData(
-                          toY: (5 + (index % 7)).toDouble(),
-                          color: Color(0xFF12005F),
+                          toY: chartData[index],
+                          color: Color(0xFF12005F), 
                           width: 15,
                           borderRadius: BorderRadius.circular(10),
                           backDrawRodData: BackgroundBarChartRodData(
                             color: Color(0xFFEEEEFC),
                             show: true,
-                            toY: 10
+                            toY: getMaxY()
                           )
                         )
                       ]
